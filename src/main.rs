f89f13e759336b41e09bf1ba38c5dfdc660d26ba
neonew <neonew@gmail.com>
2023-06-23 19:23:43 +0200

    Check existence of files

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,27 @@
+use std::path::Path;
+
 use clap::Parser;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
     #[structopt(index = 1, required = true)]
-    files: Vec<String>
+    files: Vec<String>,
 }
 
 fn main() {
     let args = Args::parse();
-    println!("Hello {:?}!", args.files)
+    println!("Files {:?}!", args.files);
+
+    let non_existing_files: Vec<_> = args.files.iter().filter(|file| {
+        let path = Path::new(file);
+        !path.exists() || !path.is_file()
+    }).collect();
+
+    if !non_existing_files.is_empty() {
+        println!("File does not exist: {:?}", non_existing_files);
+        return;
+    }
+
+    println!("All files exist: {:?}", args.files)
 }
