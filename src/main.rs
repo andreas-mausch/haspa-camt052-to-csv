b4e437700dc77e2cd32121d3c203aa9daeee12de
neonew <neonew@gmail.com>
2023-06-23 19:15:12 +0200

    Pass files as command line arguments

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,11 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    #[arg(short, long)]
-    name: String,
-
-    #[arg(short, long, default_value_t = 1)]
-    count: u8,
+    #[structopt(index = 1, required = true)]
+    files: Vec<String>
 }
 
 fn main() {
     let args = Args::parse();
-
-    for _ in 0..args.count {
-        println!("Hello {}!", args.name)
-    }
+    println!("Hello {:?}!", args.files)
 }
