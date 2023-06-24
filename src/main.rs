07688c841d1d172aa9849d07a41d678eb08079ee
neonew <neonew@gmail.com>
2023-06-24 02:08:10 +0200

    Detect mime type of file

--- src/main.rs
+++ src/main.rs
@@ -16,16 +16,25 @@ fn main() {
 
     let args = Args::parse();
     info!("Files {:?}!", args.files);
+    let paths: Vec<_> = args.files.iter().map(|file| Path::new(file)).collect();
 
-    let non_existing_files: Vec<_> = args.files.iter().filter(|file| {
-        let path = Path::new(file);
-        !path.exists() || !path.is_file()
-    }).collect();
+    let non_existing_files: Vec<_> = paths.iter().filter(|path| { !path.exists() || !path.is_file() }).collect();
 
     if !non_existing_files.is_empty() {
         error!("File does not exist: {:?}", non_existing_files);
         return;
     }
 
-    info!("All files exist: {:?}", args.files)
+    info!("All files exist: {:?}", args.files);
+
+    paths.iter().for_each(|path| {
+        match tree_magic_mini::from_filepath(path) {
+            Some("application/zip") => {
+                println!("Zip file: {:?}", path)
+            }
+            _ => {
+                println!("No zip file: {:?}", path)
+            }
+        }
+    });
 }
