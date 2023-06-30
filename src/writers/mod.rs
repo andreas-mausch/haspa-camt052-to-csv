13ba28bf1337928ff7868a44772f945f50680b14
neonew <neonew@gmail.com>
2023-06-30 12:28:17 +0200

    First .ods implementation, not writing the real transactions yet

--- src/writers/mod.rs
+++ src/writers/mod.rs
@@ -4,6 +4,7 @@ use std::io::Write;
 use crate::transaction::Transaction;
 
 pub mod csv;
+pub mod ods;
 
 pub trait Writer {
     fn write<W: Write>(transactions: &Vec<Transaction>, write: W) -> Result<(), Box<dyn Error>>;
--- src/writers/ods.rs
+++ src/writers/ods.rs
@@ -1,15 +1,42 @@
 use std::error::Error;
+use std::io::Write;
 
-use csv::WriterBuilder;
+use chrono::NaiveDate;
+use color::Rgb;
+use icu_locid::locale;
+use spreadsheet_ods::{CellStyle, format, formula, mm, Sheet, WorkBook};
+use spreadsheet_ods::style::units::{Border, Length, TextRelief};
 
 use crate::transaction::Transaction;
+use crate::writers::Writer;
 
-pub fn to_csv(transactions: Vec<Transaction>) -> Result<String, Box<dyn Error>> {
-    let mut writer = WriterBuilder::new().has_headers(true).delimiter(b';').from_writer(vec![]);
-    writer.serialize(("Date", "Valuta", "Amount", "Currency", "Creditor Name", "Creditor IBAN", "Debtor Name", "Debtor IBAN", "Transaction Type", "Description"))?;
-    transactions.iter().try_for_each(|transaction| {
-        writer.serialize(transaction)
-    })?;
-    writer.flush()?;
-    Ok(String::from_utf8(writer.into_inner()?)?)
+pub struct Ods {}
+
+impl Writer for Ods {
+    fn write<W: Write>(_transactions: &Vec<Transaction>, mut write: W) -> Result<(), Box<dyn Error>> {
+        let mut workbook = WorkBook::new(locale!("de_DE"));
+        let mut sheet = Sheet::new("Sheet");
+
+        let date_format = format::create_date_dmy_format("date_format");
+        let date_format = workbook.add_datetime_format(date_format);
+
+        let mut date_style = CellStyle::new("nice_date_style", &date_format);
+        date_style.set_font_bold();
+        date_style.set_font_relief(TextRelief::Engraved);
+        date_style.set_border(mm!(0.2), Border::Dashed, Rgb::new(192, 72, 72));
+        let date_style_ref = workbook.add_cellstyle(date_style);
+
+        sheet.set_value(0, 0, 21.4f32);
+        sheet.set_value(0, 1, "foo");
+        sheet.set_styled_value(0, 2, NaiveDate::from_ymd_opt(2020, 03, 01), &date_style_ref);
+        sheet.set_formula(0, 3, format!("of:={}+1", formula::fcellref(0, 0)));
+
+        workbook.push_sheet(sheet);
+
+        let output_vector = vec![];
+        spreadsheet_ods::write_ods_buf(&mut workbook, output_vector)
+            .map_err(|e| e.into())
+            .and_then(|vector| write.write_all(&vector)
+                .map_err(|e| e.into()))
+    }
 }
