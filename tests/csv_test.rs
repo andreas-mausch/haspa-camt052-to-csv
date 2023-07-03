934797e6a54eda6f67f0a53e828cd9e23503509b
neonew <neonew@gmail.com>
2023-07-03 13:44:59 +0200

    Simplify boxing of output buffer

--- tests/csv_test.rs
+++ tests/csv_test.rs
@@ -1,16 +1,12 @@
-use std::io::Write;
 use std::str::from_utf8;
 
 use haspa_camt052_to_csv::Format::Csv;
 use haspa_camt052_to_csv::process;
 
 #[test]
-fn test_add() {
+fn test_csv() {
     let mut buf = Vec::new();
-    {
-        let mut x: Box<dyn Write> = Box::new(&mut buf);
-        process(vec!["./tests/camt052.xml".to_string()], Csv, &mut x).unwrap();
-    }
+    process(vec!["./tests/camt052.xml".to_string()], Csv, &mut Box::new(&mut buf)).unwrap();
 
     let expected_output = std::fs::read_to_string("./tests/camt052.csv").unwrap();
     assert_eq!(from_utf8(&buf).unwrap(), expected_output, "CSV Output");
