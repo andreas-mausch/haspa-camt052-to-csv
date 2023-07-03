d2d008287eef968c1141ff4d8cc7abb93f1c08bc
neonew <neonew@gmail.com>
2023-07-03 13:45:50 +0200

    Further simplification of passing the buffer

--- tests/csv_test.rs
+++ tests/csv_test.rs
@@ -6,7 +6,7 @@ use haspa_camt052_to_csv::process;
 #[test]
 fn test_csv() {
     let mut buf = Vec::new();
-    process(vec!["./tests/camt052.xml".to_string()], Csv, &mut Box::new(&mut buf)).unwrap();
+    process(vec!["./tests/camt052.xml".to_string()], Csv, &mut buf).unwrap();
 
     let expected_output = std::fs::read_to_string("./tests/camt052.csv").unwrap();
     assert_eq!(from_utf8(&buf).unwrap(), expected_output, "CSV Output");
