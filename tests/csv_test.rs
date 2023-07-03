923b5efc99e78205fd9858b54b498319dd313cce
neonew <neonew@gmail.com>
2023-07-03 13:46:31 +0200

    Rename variable

--- tests/csv_test.rs
+++ tests/csv_test.rs
@@ -5,9 +5,9 @@ use haspa_camt052_to_csv::process;
 
 #[test]
 fn test_csv() {
-    let mut buf = Vec::new();
-    process(vec!["./tests/camt052.xml".to_string()], Csv, &mut buf).unwrap();
+    let mut output = Vec::new();
+    process(vec!["./tests/camt052.xml".to_string()], Csv, &mut output).unwrap();
 
     let expected_output = std::fs::read_to_string("./tests/camt052.csv").unwrap();
-    assert_eq!(from_utf8(&buf).unwrap(), expected_output, "CSV Output");
+    assert_eq!(from_utf8(&output).unwrap(), expected_output, "CSV Output");
 }
